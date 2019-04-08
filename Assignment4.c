937fb116fddfc5b30e0ad117c7028ce360570750
Ilia <qkunder@gmail.com>
2019-04-08 03:30:53 -0700

    Pre released version
    Fixed errors and tested

--- Assignment4.c
+++ Assignment4.c
@@ -54,6 +54,7 @@ int NumberOfVisitors(sem_t * sem, int * value)
 void try_wait(sem_t *sem){
     if (sem_wait(sem) < 0){
         printf("failed: cannot wait semaphore, terminating program\n");
+        fflush(stdout);
         exit(0);
     }
 }
@@ -61,6 +62,7 @@ void try_wait(sem_t *sem){
 void try_signal(sem_t *sem){
     if (sem_post(sem) < 0){
         printf("failed: semaphore cannot signal, terminating program\n");
+        fflush(stdout);
         exit(0);
     }
 }
@@ -85,33 +87,42 @@ int smaugRoutine()
     {
         // Smaug is sleeping after playing/fighting
         printf("Smaug goes to sleep... \n");
+        fflush(stdout);
         try_wait(visitorCount);
         printf("Smaug wakes up... \n");
+        fflush(stdout);
         printf("Smaug takes a deep breath... \n");
+        fflush(stdout);
         
         sem_getvalue(thiefCount, &enchantedThiefsCount);
         printf("Smaug smells %i Thiefs...\n", enchantedThiefsCount);
+        fflush(stdout);
 
         sem_getvalue(hunterCount, &enchantedHuntersCount);
         printf("Smaug smells %i Bounty Hunters...\n", enchantedHuntersCount);
+        fflush(stdout);
         
         // If Thief is present, play with a Thief
         if (enchantedThiefsCount > 0)
         {
             printf("Smaug plays with a Thief... \n");
+            fflush(stdout);
             try_signal(thiefPlay);
             try_wait(smaugSleep);
             if (PlayFight())
             {
               smaugJewels -= 8;
               printf("Smaug has lost the game against a Thief...\n");
+              fflush(stdout);
             }
             else
             {
               smaugJewels += 20;
               defeatedThiefs += 1;
               printf("Smaug has won the game against a Thief...\n");
-              printf("Smaug has defated %i Thiefs...\n", defeatedThiefs);	
+              fflush(stdout);
+              printf("Smaug has defated %i Thiefs...\n", defeatedThiefs);
+              fflush(stdout);	
             }
             printf("Smaug's jewels count: %i...\n", smaugJewels);
         }     
@@ -119,21 +130,26 @@ int smaugRoutine()
         else
         {
             printf("Smaug fights with a Bounty Hunter... \n");
+            fflush(stdout);
             try_signal(hunterFight);
             try_wait(smaugSleep);
             if (PlayFight()) 
             {
               smaugJewels -= 10;
               printf("Smaug has lost the fight against a Bounty Hunter...\n");
+              fflush(stdout);
             }
             else
             {
               smaugJewels += 5;
               defeatedHunters += 1;
               printf("Smaug has won the fight against Bounty Hunter...\n");
-              printf("Smaug has defated %i Bounty Hunters...\n", defeatedHunters);	
+              fflush(stdout);
+              printf("Smaug has defated %i Bounty Hunters...\n", defeatedHunters);
+              fflush(stdout);	
             }
             printf("Smaug's jewels count: %i...\n", smaugJewels);
+            fflush(stdout);
         }
     }
     
@@ -142,16 +158,18 @@ int smaugRoutine()
     else if (smaugJewels <= 0) {printf("Smaug has lost his treasure! \n");}
     else if (defeatedThiefs >=3 ) {printf("Smaug has defeated 3 Thiefs! \n");}
     else {printf("Smaug has defeated 4 Bounty Hunters! \n");}
-    printf("*************************************************\n");
-    printf("************* SMAUG TERMINATES HERE *************\n");
-    printf("*************************************************\n");
+    fflush(stdout);
+    printf("*************************************************\n************* SMAUG TERMINATES HERE *************\n*************************************************\n");
+    fflush(stdout);
     try_wait(smaugExist);
 
     sem_getvalue(thiefCount, &enchantedThiefsCount);
     printf("%i Thiefs remain in the valley. LEAVE NOW!...\n", enchantedThiefsCount);
+    fflush(stdout);
 
     sem_getvalue(hunterCount, &enchantedHuntersCount);
     printf("%i Bounty Hunters remain in the valley. LEAVE NOW!...\n", enchantedHuntersCount);
+    fflush(stdout);
     
     for (int i=0; i<enchantedThiefsCount; i++)
     {
@@ -173,19 +191,23 @@ int thiefRoutine()
 {
   int smaugAlive; 
   printf("Thief %i enters the valley...\n",getpid());
+  fflush(stdout);
   try_signal(thiefCount);
-	printf("Thief %i becomes enchanted...\n",getpid());
   try_signal(visitorCount);	
-	try_wait(thiefPlay);
-	try_signal(smaugSleep);
   sem_getvalue(smaugExist, &smaugAlive);
   if (!smaugAlive)
   {
-    printf("Thief %i leaves the valley...\n", getpid());
+    printf("Smaug is not here anymore: Thief %i leaves the valley...\n", getpid());
+    fflush(stdout);
     exit(0);
   }
+  printf("Thief %i becomes enchanted...\n",getpid());
+  fflush(stdout);
+  try_wait(thiefPlay);
+	try_signal(smaugSleep);
   try_wait(thiefCount);
 	printf("Thief %i leaves the valley...\n", getpid());
+  fflush(stdout);
 	exit(0);
 }
 
@@ -194,19 +216,23 @@ int hunterRoutine()
 {
   int smaugAlive;
   printf("Hunter %i enters the valley...\n", getpid());
+  fflush(stdout);
   try_signal(hunterCount);
-  printf("Hunter %i becomes enchanted...\n", getpid());
   try_signal(visitorCount);
   sem_getvalue(smaugExist, &smaugAlive);
   if (!smaugAlive)
   {
-    printf("Bounty Hunter %i leaves the valley...\n", getpid());
+    printf("Smaug is not here anymore: Hunter %i leaves the valley...\n", getpid());
+    fflush(stdout);
     exit(0);
   }
+  printf("Hunter %i becomes enchanted...\n", getpid());
+  fflush(stdout);
 	try_wait(hunterFight);
 	try_signal(smaugSleep);
   try_wait(hunterCount);
   printf("Bounty Hunter %i leaves the valley...\n", getpid());
+  fflush(stdout);
 	exit(0);
 }
 
@@ -275,23 +301,15 @@ int main()
 {
     srand(seed);
     // Request user to enter winProb, maximumThiefInterval, maximumHunterInterval
-    /*
     printf("Enter winProb (0-100): ");
     scanf("%d", &winProb);
-    printf("\nEnter maximumThiefInterval in msec: ");
+    printf("\nEnter maximumThiefInterval in miliseconds: ");
     scanf("%d", &maximumThiefInterval);
-    printf("\nEnter maximumHunterInterval in msec: ");
+    maximumThiefInterval = 1000*maximumThiefInterval;
+    printf("\nEnter maximumHunterInterval in miliseconds: ");
     scanf("%d", &maximumHunterInterval);
+    maximumHunterInterval = 1000*maximumHunterInterval;
     printf("\n");
-    */
-
-    winProb = 0;
-    maximumThiefInterval = 1000000;
-    maximumHunterInterval = 1000000;
-
-    
-    // initialize mutexes
-    // TO DO
     
     // initialize semaphroes
     visitorCount = sem_open(visitorCountName, O_CREAT, 0600, 0);
@@ -314,16 +332,15 @@ int main()
     {
         perror("Failed to initialize Smaug. Fork return value <= 0. \n");
         exit(EXIT_FAILURE);
-    }
-    
-    // Smaug
+    }  
     else if (smaugId == 0)
     {
+        // *** Executed only by Smaug process
         try_signal(smaugExist);
         smaugRoutine(); // smaug will terminate itself in smaugRoutine
     }
 	
-    // Generate Thiefs and Hunters
+    // Initialize Generate
     pid_t generateHuntersId = fork();
     if(generateHuntersId<0){
         perror("Failed to initialize generateHunters. Fork return value <= 0. \n");
@@ -345,18 +362,18 @@ int main()
     // wait until childrem processes have terminated before moving on in the code
     int exit_status;
     pid_t wpid;
-    while ((wpid = wait(&exit_status)) > 0){};  
+    while ((wpid = waitpid(-1, &exit_status, WNOHANG)) >= 0){}; 
 
     // destroy mutexes
     // TO DO
     // destroy semaphroes
-    sem_close (visitorCount);
-    sem_close (thiefCount);
-    sem_close (hunterCount);
-    sem_close (thiefPlay);
-    sem_close (hunterFight);
-    sem_close (smaugSleep);
-    sem_close (smaugExist);
+    sem_unlink (visitorCountName);
+    sem_unlink (thiefCountName);
+    sem_unlink (hunterCountName);
+    sem_unlink (thiefPlayName);
+    sem_unlink (hunterFightName);
+    sem_unlink (smaugSleepName);
+    sem_unlink (smaugExistName);
     
     // terminate main
     exit(0);
